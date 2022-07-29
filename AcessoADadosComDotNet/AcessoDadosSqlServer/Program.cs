2c9672d4ca2be39990c7ca3e39021e6bff107195
ferraz lessa <brunolessa@live.com>
2022-07-29 20:27:22 -0300

    connection database ADO

--- AcessoADadosComDotNet/AcessoDadosSqlServer/Program.cs
+++ AcessoADadosComDotNet/AcessoDadosSqlServer/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using Microsoft.Data.SqlClient; // dotnet add package Microsoft.Data.SqlClient --version 2.1.3 (NUGET)
 
 namespace AcessoDadosSqlServer
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var connection = new SqlConnection(StringConnection.connection))
+			{
+				connection.Open();
+				using(var command = new SqlCommand())
+				{
+					command.Connection = connection;
+					command.CommandType = System.Data.CommandType.Text;
+					command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+					
+					var reader = command.ExecuteReader();
+					while (reader.Read())
+					{
+						Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+					}
+				}
+				connection.Close();
+			}
+		}
+	}
 }
