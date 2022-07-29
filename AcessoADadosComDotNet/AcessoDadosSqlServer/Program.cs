d19940f03c58e2ef184d1095436778f08ca72188
ferraz lessa <brunolessa@live.com>
2022-07-29 19:43:17 -0300

    start data access

---
+++ AcessoADadosComDotNet/AcessoDadosSqlServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AcessoDadosSqlServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
