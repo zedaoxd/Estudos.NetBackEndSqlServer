be9aeb1f3d3bc413c6893bc2f6021e4bdfa26cf9
ferraz lessa <brunolessa@live.com>
2022-07-29 21:12:35 -0300

    start connection DB with Dapper

---
+++ AcessoADadosComDotNet/DapperConnectionDB/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Dapper;
+using DapperConnectionDB.model;
+using Microsoft.Data.SqlClient;
+
+namespace DapperConnectionDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var connection = new SqlConnection(StringConnection.connection))
+            {
+                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+                foreach (var category in categories)
+                {
+                    Console.WriteLine($"{category.Id} - {category.Title}");
+                }
+            }
+        }
+    }
+}
---
+++ AcessoADadosComDotNet/DapperConnectionDB/model/Category.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace DapperConnectionDB.model
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+    }
+}
