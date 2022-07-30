1c62f57465881a55d208074e8e9743f7011595f8
ferraz lessa <brunolessa@live.com>
2022-07-29 21:43:15 -0300

    insert DB with Dapper

--- AcessoADadosComDotNet/DapperConnectionDB/Program.cs
+++ AcessoADadosComDotNet/DapperConnectionDB/Program.cs
@@ -9,12 +9,40 @@ namespace DapperConnectionDB
     {
         static void Main(string[] args)
         {
+            var category = new Category(
+                Guid.NewGuid(),
+                "Amazon AWS",
+                "amazon",
+                "AWS Cloud",
+                8,
+                "Categoria destinada a serviçoes do AWS",
+                false
+            );
+
+            // SQL injection
+            var insertSql = @"INSERT INTO 
+                    [Category] ([Id], [Title], [Url], [Summary], [Order],[Description], [Featured]) 
+                VALUES 
+                    (@Id, @Title, @Url, @Summary, @Order, @Description, @Featured)";
+
             using (var connection = new SqlConnection(StringConnection.connection))
             {
-                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-                foreach (var category in categories)
+                var rows = connection.Execute(insertSql, new
                 {
-                    Console.WriteLine($"{category.Id} - {category.Title}");
+                    category.Id,
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured
+                });
+                Console.WriteLine($"{rows} linhas inseridas!");
+
+                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+                foreach (var item in categories)
+                {
+                    Console.WriteLine($"{item.Id} - {item.Title}");
                 }
             }
         }
--- AcessoADadosComDotNet/DapperConnectionDB/model/Category.cs
+++ AcessoADadosComDotNet/DapperConnectionDB/model/Category.cs
@@ -6,5 +6,25 @@ namespace DapperConnectionDB.model
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
+        public string Url { get; set; }
+        public string Summary { get; set; }
+        public int Order { get; set; }
+        public string Description { get; set; }
+        public bool Featured { get; set; }
+
+        public Category()
+        {
+        }
+
+        public Category(Guid id, string title, string url, string summary, int order, string description, bool featured)
+        {
+            Id = id;
+            Title = title;
+            Url = url;
+            Summary = summary;
+            Order = order;
+            Description = description;
+            Featured = featured;
+        }
     }
 }
