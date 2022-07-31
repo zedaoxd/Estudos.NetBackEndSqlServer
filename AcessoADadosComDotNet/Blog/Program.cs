48e19a7d6eae2d8e028c96a1cdf3a1b61599997b
ferraz lessa <brunolessa@live.com>
2022-07-30 21:39:06 -0300

    Start Blog Project

---
+++ AcessoADadosComDotNet/Blog/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Blog.models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadUsers();
+        }
+
+        public static void ReadUsers()
+        {
+            using (var conection = new SqlConnection(StringConnection.Connection))
+            {
+                var users = conection.GetAll<User>();
+
+                foreach (var user in users)
+                {
+                    Console.WriteLine(user.Name);
+                }
+            }
+        }
+    }
+}
