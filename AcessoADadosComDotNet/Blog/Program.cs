441f8a07589f268870f3751b6a2256acbc7506d2
ferraz lessa <brunolessa@live.com>
2022-07-30 22:05:56 -0300

    CRUD with Dapper.Contrib

--- AcessoADadosComDotNet/Blog/Program.cs
+++ AcessoADadosComDotNet/Blog/Program.cs
@@ -9,7 +9,25 @@ namespace Blog
     {
         static void Main(string[] args)
         {
-            ReadUsers();
+            //ReadUsers();
+
+            //GetUser(1);
+
+            var user = new User()
+            {
+                Id = 2,
+                Bio = "Students style",
+                Email = "student@gmail.com",
+                Image = "https://",
+                Name = "Student",
+                PasswordHash = "HASH",
+                Slug = "student-time"
+            };
+            //CreateUser(user);
+
+            //UpdateUser(user);
+
+            DeleteUser(user);
         }
 
         public static void ReadUsers()
@@ -22,6 +40,42 @@ namespace Blog
                 {
                     Console.WriteLine(user.Name);
                 }
+            }
+        }
+
+        public static void GetUser(int id)
+        {
+            using (var conection = new SqlConnection(StringConnection.Connection))
+            {
+                var user = conection.Get<User>(id);
+                Console.WriteLine(user.Name);
+            }
+        }
+
+        public static void CreateUser(User user)
+        {
+            using (var conection = new SqlConnection(StringConnection.Connection))
+            {
+                conection.Insert<User>(user);
+                Console.WriteLine("Cadastro realizado com sucesso");
+            }
+        }
+
+        public static void UpdateUser(User user)
+        {
+            using (var conection = new SqlConnection(StringConnection.Connection))
+            {
+                conection.Update<User>(user);
+                Console.WriteLine("Cadastro atualizado com sucesso");
+            }
+        }
+
+        public static void DeleteUser(User user)
+        {
+            using (var conection = new SqlConnection(StringConnection.Connection))
+            {
+                conection.Delete<User>(user);
+                Console.WriteLine("Usuário excluido com sucesso");
             }
         }
     }
