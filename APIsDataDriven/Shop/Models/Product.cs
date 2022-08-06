49ab46c499046d264c4ffc0df1762beb309c7e88
ferraz lessa <brunolessa@live.com>
2022-08-06 18:56:52 -0300

    product model

---
+++ APIsDataDriven/Shop/Models/Product.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 a 60 caracteres")]
+        [MaxLength(3, ErrorMessage = "Este campo deve conter entre 3 a 60 caracteres")]
+        public string Title { get; set; }
+
+        [MaxLength(1024, ErrorMessage = "Este campo deve ter até 1024 caractares")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Range(1, int.MaxValue, ErrorMessage = "O preço deve ser maior que zero")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Range(1, int.MaxValue, ErrorMessage = "Categoria inválida")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
