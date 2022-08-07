f0c01a89f00706987bffda91d6308538e195bf4a
ferraz lessa <brunolessa@live.com>
2022-08-06 23:59:39 -0300

    dependency injection

--- APIsDataDriven/Shop/Startup.cs
+++ APIsDataDriven/Shop/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Shop.Data;
 
 namespace Shop
 {
@@ -25,6 +27,8 @@ namespace Shop
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Shop", Version = "v1" });
             });
+            services.AddDbContext<DataContext>(x => x.UseInMemoryDatabase("Database"));
+            services.AddScoped<DataContext, DataContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
